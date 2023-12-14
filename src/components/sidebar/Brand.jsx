c6964361195f3054812e3707c3c64c9203f9622a
Benedict <aliubenedict@gmail.com>
2023-12-14 03:53:33 +0100

    commit -11

---
+++ src/components/sidebar/Brand.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useGlobalContext } from "../../context";
+
+const Brand = () => {
+  const { openSidebar } = useGlobalContext();
+  return (
+    <div className="brand mb-3">
+      <span>
+        <svg width="24" height="25" xmlns="http://www.w3.org/2000/svg">
+          <g fill="#635FC7" fill-rule="evenodd">
+            <rect width="6" height="25" rx="2" />
+            <rect opacity=".75" x="9" width="6" height="25" rx="2" />
+            <rect opacity=".5" x="18" width="6" height="25" rx="2" />
+          </g>
+        </svg>
+      </span>
+      <h2>Kanban</h2>
+    </div>
+  );
+};
+
+export default Brand;
