ce0cc12809f3837126ebb0db6a7da91f91046542
Benedict <aliubenedict@gmail.com>
2023-12-16 23:41:34 +0100

    New Task Modal created- progress-13

--- src/components/modal/NewBoardModal.jsx
+++ src/components/modal/NewBoardModal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useGlobalContext } from "../../context";
+import CloseModal from "./CloseModal";
 
 const NewBoardModal = () => {
   const {
@@ -15,14 +16,7 @@ const NewBoardModal = () => {
   return (
     newBoardModal && (
       <div className={`overlay ${newBoardModal ? "active" : ""}`}>
-        <span className="close-modal" onClick={closeNewBoardModal}>
-          <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg">
-            <g fill="#828FA3" fill-rule="evenodd">
-              <path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z" />
-              <path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z" />
-            </g>
-          </svg>
-        </span>
+        <CloseModal modalToCLose={closeNewBoardModal} />
         <form
           onSubmit={handleSubmit(onSubmit)}
           className={`form-modal ${newBoardModal ? "active" : ""}`}
---
+++ src/components/modal/NewTaskModal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewTaskModal = () => {
+  return <div>NewTaskModal</div>;
+};
+
+export default NewTaskModal;
