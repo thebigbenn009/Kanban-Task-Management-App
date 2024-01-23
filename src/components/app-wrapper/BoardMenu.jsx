0099e5ba33ee7beff7f1306fbec26942257f87db
benedict <aliubenedict@gmail.com>
2024-01-23 21:42:19 +0100

    work in progress-14

---
+++ src/components/app-wrapper/BoardMenu.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { boardMenuActions } from "../../features/boardMenu/boardMenuSlice";
+
+const BoardMenu = () => {
+  const dispatch = useDispatch();
+  const isBoardMenuOpen = useSelector(
+    (state) => state.boardMenu.isBoardMenuOpen
+  );
+  const openEditBoardHandler = () => {
+    dispatch(boardMenuActions.openEditBoardModal());
+    dispatch(boardMenuActions.closeEditBoardMenu());
+  };
+  const openDeleteBoardHandler = () => {
+    dispatch(boardMenuActions.openEditBoardModal());
+    dispatch(boardMenuActions.closeEditBoardMenu());
+  };
+  return (
+    <div className={`board-menu ${isBoardMenuOpen && "board-menu-visible"}`}>
+      <p onClick={openEditBoardHandler} className="edit-task">
+        edit board
+      </p>
+      <p onClick={openDeleteBoardHandler} className="delete-task">
+        delete board
+      </p>
+    </div>
+  );
+};
+
+export default BoardMenu;
